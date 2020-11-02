bb6b62c89d11f9d4f4e8df419445be62140989e4
AishaAydin <ayseydin13@gmail.com>
2020-11-01 19:12:59 -0500

    adding message

--- src/main/java/Simba.java
+++ src/main/java/Simba.java
@@ -2,5 +2,6 @@ public class Simba {
     public static void main(String[] args) {
         System.out.println("Hello Simba");
         System.out.println("Welcome to our family");
+        System.out.println("We are very happy that you are with us");
     }
 }
