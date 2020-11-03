3100c0c5c6bc51a807da34dfed3b895ae1458721
AishaAydin <ayseydin13@gmail.com>
2020-11-03 09:30:20 -0500

    humeyra branch changes

--- src/test/java/ComingSoon.java
+++ src/test/java/ComingSoon.java
@@ -8,5 +8,6 @@ public class ComingSoon {
 
         System.out.println("This changes from ayse branch");
         System.out.println("One more adding to ayse branch");
+        System.out.println("I didi this changes from humeyra branch");
     }
 }
